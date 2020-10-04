0776379f4bad28bda656317caa223995ee4aa202
Johnny-Chang-Aus <70256485+Johnny-Chang-Aus@users.noreply.github.com>
2020-10-04 13:49:34 +1100

    Update index.js
    finished the card function that displayed a card

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function Card(props) {
+  return (
+    <div className="card margin">
+      <img src={props.img} alt="card_image" />
+      <h1>{props.title}</h1>
+      <h2>{props.subTitle}</h2>
+    </div>
+  );
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <h1 className="margin">Gifts for Him</h1>
+    <Card img={cardImageGift} title="Gift for Him" subTitle="For Valentine's Day" />
+  </div>,
   document.getElementById('root')
 );
 
